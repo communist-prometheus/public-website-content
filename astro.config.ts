d4a6be12bd7409b6b206efcb1bd0daacffbc23d8
undeadliner <wemonpf@proton.me>
2026-02-08 22:12:25 +0100

    chore: setup modern linting and formatting tools

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   cacheDir: './.astro-cache',
   vite: {
-    cacheDir: './.vite-cache'
-  }
+    cacheDir: './.vite-cache',
+  },
 });
