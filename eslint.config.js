dc9e840fc8d5d87baa00ae4965714df0b775c820
undeadliner <wemonpf@proton.me>
2026-02-10 21:50:07 +0100

    chore: add ESLint with strict comment rules

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import astroPlugin from 'eslint-plugin-astro';
+import jsdocPlugin from 'eslint-plugin-jsdoc';
+
+export default [
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      '.astro/**',
+      '.vite-cache/**',
+      '**/*.astro',
+      '*.config.js',
+      '*.config.ts',
+      'bun.lock',
+    ],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      jsdoc: jsdocPlugin,
+    },
+    rules: {
+      'no-inline-comments': 'error',
+      'line-comment-position': ['error', { position: 'above' }],
+      'multiline-comment-style': ['error', 'starred-block'],
+      'spaced-comment': [
+        'error',
+        'always',
+        {
+          markers: ['/'],
+          exceptions: ['*'],
+          block: {
+            balanced: true,
+          },
+        },
+      ],
+      'jsdoc/require-jsdoc': [
+        'warn',
+        {
+          publicOnly: true,
+          require: {
+            FunctionDeclaration: true,
+            ClassDeclaration: true,
+            MethodDefinition: true,
+          },
+        },
+      ],
+      'jsdoc/check-alignment': 'error',
+      'jsdoc/check-indentation': 'error',
+    },
+  },
+];
